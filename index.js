741fd35258d8a292287b402249019d58c3087926
Gary Sieling <gary@garysieling.com>
2018-10-21 19:02:27 -0400

    Match format of a lambda

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const AWS = require('aws-sdk'),
       fs = require('fs');
 
 const S3 = require('aws-sdk/clients/s3');
-
-const id = '1';
-const data = {title_s: 'test'};
-
 const s3 = new AWS.S3();
 
 const exec = require('child_process').exec;
 const dataDir = process.env.DATA_DIR;
 const ffmpegDir = './exodus/bin/ffmpeg';
-const ytId = 'tPaLBW1uoTg';
-const url = 'https://www.youtube.com/watch?v=' + ytId;
 
-exec(`./bin/youtube-dl \
+function run(event, context, callback) {
+  const ytId = event.ytId;
+  const url = 'https://www.youtube.com/watch?v=' + ytId;
+
+  exec(`./bin/youtube-dl \
       --ffmpeg-location ${ffmpegDir} \
       --skip-download \
       --ignore-errors --youtube-skip-dash-manifest \
@@ -22,20 +20,25 @@ exec(`./bin/youtube-dl \
       --no-call-home \
       ${url}
 	`, function callback(error, stdout, stderr){
-  if (error) {
-    console.log(error);
-  }
+    if (error) {
+      console.log(error);
+    }
 
-  console.log(stdout);
-  console.log(stderr);
+    console.log(stdout);
+    console.log(stderr);
 
-  const data = fs.readFileSync(dataDir + '/data.info.json');
-  s3.putObject({
-    Bucket: 'findlectures',
-    Key: 'v' + ytId + '.json',
-    Body: new Buffer(data, 'binary')
-  },function (resp) {
-    console.log(arguments);
-    console.log('Successfully uploaded package.');
+    const data = fs.readFileSync(dataDir + '/data.info.json');
+    s3.putObject({
+      Bucket: 'findlectures',
+      Key: 'v' + ytId + '.json',
+      Body: new Buffer(data, 'binary')
+    },function (resp) {
+      console.log(arguments);
+      console.log('Successfully uploaded package.');
+
+      callback(null, "Success!");
+    });
   });
-})
+}
+
+exports.index = run;
